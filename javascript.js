e28a6854ae664d78358c349b6346320393c693a8
felipe <felipeggrs@gmail.com>
2023-06-05 19:01:11 -0300

    Update CSS, still WIP

--- javascript.js
+++ javascript.js
@@ -2,14 +2,20 @@
 const playerFactory = () => {
   const playerSelection = "X";
   const gameContainer = document.getElementById("gameContainer");
+
   const selectionX = document.createElement("button");
   selectionX.textContent = "X";
+  selectionX.setAttribute("style", "display: none");
   gameContainer.appendChild(selectionX);
+
   const selectionO = document.createElement("button");
   selectionO.textContent = "O";
+  selectionO.setAttribute("style", "display: none");
   gameContainer.appendChild(selectionO);
+
   const gameReset = document.createElement("button");
   gameReset.textContent = "Reset";
+  gameReset.setAttribute("style", "display: none");
   gameContainer.appendChild(gameReset);
 
   return { playerSelection, selectionX, selectionO, gameReset };
@@ -32,11 +38,15 @@ const Gameboard = (function () {
       // 'X' button: player chooses X
       player.selectionX.addEventListener("click", () => {
         player.playerSelection = "X";
+        player.selectionX.setAttribute("style", "display: none");
+        player.selectionO.setAttribute("style", "display: none");
       });
 
       // 'O' button: player chooses O
       player.selectionO.addEventListener("click", () => {
         player.playerSelection = "O";
+        player.selectionX.setAttribute("style", "display: none");
+        player.selectionO.setAttribute("style", "display: none");
       });
 
       // square click functionality
@@ -44,6 +54,11 @@ const Gameboard = (function () {
         if (gameOver) {
           return; // Ignore click if the game is already over
         }
+
+        // hide selection after game has started
+        player.selectionX.setAttribute("style", "display: none");
+        player.selectionO.setAttribute("style", "display: none");
+
         // change array items "" to playerSelection and display them on the page
         board[i] = player.playerSelection;
         gameSquare.textContent = board[i];
@@ -94,6 +109,9 @@ const Gameboard = (function () {
         player.playerSelection = "X";
         gameSquare.textContent = board[i];
         gameSquare.addEventListener("click", squareClick);
+
+        player.selectionX.setAttribute("style", "display: inline-block");
+        player.selectionO.setAttribute("style", "display: inline-block");
       });
 
       gameSquare.addEventListener("click", squareClick);
@@ -102,4 +120,22 @@ const Gameboard = (function () {
   return { game };
 })();
 
-Gameboard.game();
+const startGame = document.getElementById("startGame");
+
+function start() {
+  player.selectionX.setAttribute("style", "display: inline-block");
+  player.selectionO.setAttribute("style", "display: inline-block");
+  player.gameReset.setAttribute("style", "display: inline-block");
+  Gameboard.game();
+  startGame.setAttribute("style", "display: none");
+  const gameContainer = document.getElementById("gameContainer");
+  const selectWeapon = document.createElement("h1");
+  selectWeapon.textContent = "Select your weapon!";
+  selectWeapon.id = "selectWeapon";
+  gameContainer.insertBefore(
+    selectWeapon,
+    gameContainer.firstChild.nextSibling.nextSibling
+  );
+}
+
+startGame.addEventListener("click", start);
